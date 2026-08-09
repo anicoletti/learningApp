0a7dc09c772a1019e2133bdeb42741e3567202c0
Alex Nicoletti <alex.nicoletti@gmail.com>
2026-08-09 19:44:38 -0400

    Clip overflowing background in Level1Screen

--- apps/solarSystem/screens/Level1Screen.tsx
+++ apps/solarSystem/screens/Level1Screen.tsx
@@ -276,6 +276,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000',
+    overflow: 'hidden',
   },
   darkOverlay: {
     ...StyleSheet.absoluteFillObject,
